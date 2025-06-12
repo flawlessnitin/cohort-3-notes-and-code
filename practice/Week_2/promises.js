9dc43085bd4d83c6a6816abe9a26434d19232032
Nitin Sahu <sahunitin970@gmail.com>
2025-06-12 16:23:54 +0530

    2 assignment of week 2 and custom promise class

--- practice/Week_2/promises.js
+++ practice/Week_2/promises.js
@@ -1,18 +1,49 @@
-class Rectangle {
-  constructor(width, height, color) {
-    this.width = width;
-    this.height = height;
-    this.color = color
+// class Rectangle {
+//   constructor(width, height, color) {
+//     this.width = width;
+//     this.height = height;
+//     this.color = color
+//   }
+//   area() {
+//     const area = this.width * this.height;
+//     return area;
+//   }
+//   paint() {
+//     console.log(`Painting with color ${this.color}`)
+//   }
+// }
+
+// const obj = new Rectangle(10, 20, 30);
+// const area = obj.area();
+// console.log("Area:- ", area)
+
+// Writing our own promise
+// First have reference a little bit from /assignments/week_2/promisifiedCleanFile.js
+class Promise2 {
+  constructor(fn) {
+    function afterDone() {
+      this.resolve();
+    }
+    fn(afterDone);
   }
-  area() {
-    const area = this.width * this.height;
-    return area;
-  }
-  paint() {
-    console.log(`Painting with color ${this.color}`)
+  then(callback) {
+    this.resolve = callback;
   }
 }
+function readTheFile(resolve) {
+  setTimeout(function () {
+    console.log("callback based setTimeout completed");
+    resolve();
+  }, 3000);
+}
 
-const obj = new Rectangle(10, 20, 30);
-const area = obj.area();
-console.log("Area:- ", area)
+function setTimeoutPromisified() {
+  return new Promise2(readTheFile);
+}
+
+let p = setTimeoutPromisified();
+function callback() {
+  console.log("Callback has been called!!");
+}
+
+p.then(callback);
